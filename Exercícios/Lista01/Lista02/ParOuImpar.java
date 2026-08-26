c94762c6243d1b053c50f700c0d0bad783dd8b6d
Giovanna Stypurski <stypurskigiovanna@gmail.com>
2026-08-26 13:35:46 +0000

    subir codigo

---
+++ Exercícios/Lista01/Lista02/ParOuImpar.java
@@ -0,0 +1,23 @@
+package exercicios.exercicio01;
+
+import java.util.scanner; 
+
+public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Insira seu numero: ");
+    int numero = sc.nextint();
+
+
+    int resultado = numero % 2 ;
+
+    if(resultado == 0){
+        System.out.println("O numero digitado e par");
+    }else{
+        System.out.println("O numero digitado e par");
+    }
+
+    sc.close();
+
+}
